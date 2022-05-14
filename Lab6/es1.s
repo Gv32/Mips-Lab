451364662aa5dd92dd9f115790f4fd6a29830c69
Gv32 <s284383@studenti.polito.it>
2022-05-14 19:55:20 +0200

    Lab 6 started

--- Lab6/es1.s
+++ Lab6/es1.s
@@ -1,7 +1,7 @@
                 .data
 
-term:           .asciiz '\n'
-ast:            .asciiz '*'
+term:           .asciiz "0"
+ast:            .asciiz "*"
 
                 .text
                 .globl main
@@ -9,15 +9,43 @@ ast:            .asciiz '*'
 
 stampaTriangolo:
     
-    ciclo1:
+    ciclo1t:
 
-        ciclo2:
+        ciclo2t:
 
 
 
 stampaQuadrato:
+    addi $t0, $t0, 0                    # Indice 1
+    addi $t1, $t1, 0                    # Indice 2
+    move $t2, $a0                       # Asterisco
+    move $t3, $a1                       # \n
+    ciclo1q:
+
+        li $v0, 4
+        move $a0, $t0
+        ciclo2q:
+            syscall
+            addi $t1, $t1, 1
+        bne $t1, 8, ciclo2q
+    
+    bne $t0, 8, ciclo1q
+
 
 
 main:
+    la $t0, ast
+    la $t1, term
+    li $v0, 4
+    move $a0, $t0
+    syscall
+    li $v0, 4
+    move $a0, $t1
+    syscall
+    li $v0, 4
+    move $a0, $t0
+    syscall
+
+    jal stampaQuadrato
 
 .end main
---
+++ Lab6/es4.s
@@ -0,0 +1,35 @@
+                .data
+vet:            .word 1, 2, 3, 5, 4, 6, 5, 8, 4, 5
+n:              .word 10
+msg:            .asciiz "Il massimo e': "
+
+                .text
+
+                .globl main
+                .ent main
+
+massimo:
+    addi $t0, $t0, 0                    #indice
+    addi $t1, $t1, 0                    #massimo
+    mul $a1, $a1, 4
+    for:
+        bgt $a0($t0), $t1, max
+        j nm
+
+    max:
+        move $t1, $a1($t0)
+    nm:
+        addi $t0, $t0, 4
+        bne $t0, $a1, for
+    move $v0, $t1
+    j $ra
+
+
+main:
+    lw $a0, vet
+    lw $a1, n
+    jal massimo
+
+
+
+.end main
