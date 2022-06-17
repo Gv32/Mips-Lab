ed40bd0ec4365f69b69987648cd56b2bb04f4b79
Gv32 <giovannoni1602@gmail.com>
2022-06-17 16:50:31 +0200

    es1 started, es3 started

---
+++ Lab8/es1.s
@@ -0,0 +1,37 @@
+                .data
+vettoreEntrate: .byte 12, 47   
+vettoreUscite:  .byte 18, 14
+X:              .byte 1
+Y:              .byte 40
+                .text
+                .globl main
+                .ent main
+
+main:
+    sub $sp, $sp, 4
+    sw $ra, ($sp)
+    la $a0, vettoreEntrate      # indirizzo di vettoreEntrate
+    la $a1, vettoreUscite       # indirizzo di vettoreUscite
+    lbu $a2, X
+    lbu $a3, Y
+    jal costoParcheggio
+    lw $ra, ($sp)
+    addiu $sp, $sp, 4
+    jr $ra
+    li $v0, 10
+    syscall
+.end main
+
+costoParcheggio:
+    lbu $t0, ($a1)      # carico il primo elemento di vettoreUscite
+    lbu $t1, ($a0)      # carico il primo elemento di vettoreEntrate
+    subu $t0, $t0, $t1  # calcolo la differenza
+    li $t1, 60          # 60 minuti
+    multu $t0, $t1      # t0 * t1
+    mflo $t3            # converto le ore in minuti
+    lbu $t0, 1($a1)     # carico il secondo elemento di vettoreUscite
+    lbu $t1, 1($a0)     # carico il secondo elemento di vettoreEntrate
+    subu $t4, $t0, $t1  # calcolo la differenza
+    bge $t4
+    jr $ra
+.end costoParcheggio
---
+++ Lab8/es3.s
@@ -0,0 +1,18 @@
+                .data
+
+vettorePrezzi   .byte
+
+                .globl main
+                .ent main
+
+main:
+
+
+.end main
+
+
+calcola_sconto:
+
+
+
+.end calcola_sconto
